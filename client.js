5e7dd9a0d54211330fb58cfbe039040c06042eb8
Ia Vue <iavue00@gmail.com>
2023-02-24 15:18:08 -0600

    created content in index.html. created onready function with listener for the submit button and started the addSalary button when called by the listener

--- client.js
+++ client.js
@@ -1,5 +1,15 @@
 $(document).ready(readyNow);
 
+let form = []; // might change the name later
+
 function readyNow() {
     console.log("DOM is loaded!");
+
+    // Listener for submit button
+    $('#submitBtn').on('click', addSalary)
+}
+
+function addSalary() {
+    console.log('Inside of addSalary');
+    
 }
