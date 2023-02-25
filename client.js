97522e2f9ebdcbc7fbd441ff32806ec4d14bc0bf
Ia Vue <iavue00@gmail.com>
2023-02-24 23:09:06 -0600

    fixed table in render and in the middle of figuring out calculation

--- client.js
+++ client.js
@@ -13,7 +13,7 @@ function addSalary() {
     console.log('Inside of addSalary');
     console.log('Form[] before input is added:', form);
 
-    // Get input values
+    // Get input values and store in variables
     const firstNameValue = $('#firstNameInput').val();
     const lastNameValue = $('#lastNameInput').val();
     const idNumValue = $('#idNumInput').val();
@@ -27,24 +27,33 @@ function addSalary() {
         Annual Salary: ${annualSalInput}
         `);
     
-    const newInputToAdd = {
-        firstName: firstNameValue,
-        lastName: lastNameValue,
-        idNum: idNumValue,
-        jobTitle: jobTitleValue,
-        annualSal: annualSalValue
-    }
+    if (firstNameValue && lastNameValue &&
+        idNumValue && jobTitleValue && annualSalValue) {
+            
+        const newInputToAdd = {
+            firstName: firstNameValue,
+            lastName: lastNameValue,
+            idNum: idNumValue,
+            jobTitle: jobTitleValue,
+            annualSal: annualSalValue
+        }
     
-    // add object (newInputToAdd) to form[]
-    form.push(newInputToAdd);
-    console.log('Form[] after new input added:', form);
+        // add object (newInputToAdd) to form[]
+        form.push(newInputToAdd);
+        console.log('Form[] after new input added:', form);
 
-    // Render the DOM after new input is added
-        // Re-render when new car is added
-    render();
+        // Render the DOM after new input is added
+         // Re-render when new car is added
+        render();
 
-    // Reset input fields
-    resetInputFields();
+         // Reset input fields
+        resetInputFields();
+
+        // Calculate Monthly Costs
+        calcMonthlyCosts();
+    } else {
+        alert("Inputs are invalid.")
+    }
 }
 
 function resetInputFields() {
@@ -58,12 +67,23 @@ function resetInputFields() {
 
 function render() {
     // Update the DOM with the input values
+
     // Reset the table where the inputs are/will be
-    $('#employeeInfo').empty()
+    $('#tableOfInputs').empty()
+
+    $('#tableOfInputs').append(`
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>ID Number</th>
+                <th>Job Title</th>
+                <th>Annual Salary</th>
+            </tr>
+            `);
 
     for(let input of form){
         console.log('New input of the employee salary info:', input);
-        $('#salaryTable').append(`
+        $('#tableOfInputs').append(`
             <tr>
                 <td>${input.firstName}</td>
                 <td>${input.lastName}</td>
@@ -76,4 +96,50 @@ function render() {
             </tr>
         `);
     }
+    
+}
+
+/* Using the stored information, calculate monthly costs and append this to 
+the DOM. If the total monthly cost exceeds $20,000, add a red background 
+color to the total monthly cost. 
+
+- Target all of the annual salary properties from the form[], 
+- add them together then divide by 12
+- then append to #totalMonthly table
+- If the total > 20000
+- make #totalMonthly table in to a red background color
+
+*/
+
+function calcMonthlyCosts() {
+    console.log('Inside calcMonthlyCosts()');
+
+    $('#totalMonthly').empty()
+
+    $('#totalMonthly').append(`
+            <tr>
+                <td>Total Monthly:</t>
+            </tr>
+            `);
+    
+    // Create new object of salaries
+    let salaries = {};
+    console.log('The salaries object before adding input:', salaries);
+    form.salary = $('#annualSalInput').val();
+    // Get the input of salaries
+    // const annualSalValue = form.annualSal
+    for (let i = 0; i < form.length; i++) {
+        const element = form[i];
+        
+    }
+
+    // Put the input salary in to the salaries array
+    salaries = annualSalValue
+    console.log('The salaries array after adding input:', salaries);
+
+    // for (let {annualSal} of form) {
+    //     console.log('annualSal property:', annualSal);
+    //     let allSalaries = [];
+
+    // }
 }
